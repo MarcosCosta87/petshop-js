3772f62e37ad868f0dd840ba2cd94120c34a5709
Marcos Fernando Da Costa Santos <marcos.fernando@linx.com.br>
2021-04-14 20:26:16 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const nomePetshop = "PETSHOP DH *****";
+
+let pets = [
+    {
+        nome: 'Bob',
+        idade: 2,
+        raca: 'pug',
+        tipo: 'cachorro',
+        vacinado: true,
+        genero: 'M',
+        servicos: ['banho', 'tosa']
+    },
+    {
+        nome: 'Snoopy',
+        idade: 4,
+        raca: 'pug',
+        tipo: 'cachorro',
+        vacinado: true,
+        genero: 'M',
+        servicos: ['banho']
+    },
+    {
+        nome: 'Paçoca',
+        idade: 10,
+        raca: 'vira-lata caramelo',
+        tipo: 'cachorro',
+        vacinado: true,
+        genero: 'F',
+        servicos: ['tosa']
+    }
+];
+
+//Arrow function
+const adicionarPet = (infoPet) => {
+    pets.push(infoPet);
+    
+}
+
+adicionarPet({
+    nome: 'Rex',
+    idade: 4,
+    raca: 'caramelo',
+    tipo: 'cachorro',
+    vacinado: false,
+    genero: 'M',
+    servicos: ['banho']
+});
+
+console.log(pets);
